895dcf3a6836081a1ef6d0757059ba8c89665ddf
Janne <peterdev80@gmail.com>
2020-03-02 19:12:07 +0200

    little fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { connect, useDispatch } from 'react-redux'
-import { initializer } from './reducers/countryReducer'
+import { connect, useDispatch } from 'react-redux';
+import { initializer } from './reducers/countryReducer';
 
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Region from './components/Region';
 import Menubar from './components/MenuBar';
 import Country from './components/Country';
@@ -13,51 +10,73 @@ import SideDrawer from './components/SideDrawer';
 import BackDrop from './components/BackDrop';
 import Info from './components/Info';
 
-
 interface Props {
-  initializer: any
-  countries: any
+  initializer: any;
+  countries: any;
 }
 
-
-const App: React.FC<Props> = (props) => {
-  const dispatch = useDispatch()
-  const [search, setSearch] = useState<string>('')
-  const [sideDrawerOpen, setSideDrawerOpen] = useState<boolean>(false)
+const App: React.FC<Props> = props => {
+  const dispatch = useDispatch();
+  const [search, setSearch] = useState<string>('');
+  const [sideDrawerOpen, setSideDrawerOpen] = useState<boolean>(false);
 
   useEffect(() => {
-    dispatch(initializer())
-  }, [dispatch])
+    dispatch(initializer());
+  }, [dispatch]);
 
   const toggleSideDrawer = () => {
-    setSideDrawerOpen(!sideDrawerOpen)
-  }
+    setSideDrawerOpen(!sideDrawerOpen);
+  };
 
   return (
     <div>
-      <Router basename='/countries'>
+      <Router basename="/">
         <SideDrawer sideDrawerOpen={sideDrawerOpen}></SideDrawer>
-        <BackDrop sideDrawerOpen={sideDrawerOpen} toggleSideDrawer={toggleSideDrawer}></BackDrop>
-        <div className='header'></div>
-        <Menubar toggleSideDrawer={toggleSideDrawer} setSearch={setSearch}></Menubar>
-        <Route exact path='/'><Info/></Route>
-        <Route exact path='/Africa'><Region search={search} filter='Africa' /></Route>
-        <Route exact path='/Europe'><Region search={search} filter='Europe' /></Route>
-        <Route exact path='/Americas'><Region search={search} filter='Americas' /></Route>
-        <Route exact path='/Asia'><Region search={search} filter='Asia' /></Route>
-        <Route exact path='/Oceania'><Region search={search} filter='Oceania' /></Route>
-        <Route exact path='/All'><Region search={search} filter='All' /></Route>
-        <Route exact path='/country/:name' render={(props) => <Country {...props} />} />
+        <BackDrop
+          sideDrawerOpen={sideDrawerOpen}
+          toggleSideDrawer={toggleSideDrawer}
+        ></BackDrop>
+        <div className="header"></div>
+        <Menubar
+          toggleSideDrawer={toggleSideDrawer}
+          setSearch={setSearch}
+        ></Menubar>
+        <Route exact path="/">
+          <Info />
+        </Route>
+        <Route exact path="/Africa">
+          <Region search={search} filter="Africa" />
+        </Route>
+        <Route exact path="/Europe">
+          <Region search={search} filter="Europe" />
+        </Route>
+        <Route exact path="/Americas">
+          <Region search={search} filter="Americas" />
+        </Route>
+        <Route exact path="/Asia">
+          <Region search={search} filter="Asia" />
+        </Route>
+        <Route exact path="/Oceania">
+          <Region search={search} filter="Oceania" />
+        </Route>
+        <Route exact path="/All">
+          <Region search={search} filter="All" />
+        </Route>
+        <Route
+          exact
+          path="/country/:name"
+          render={props => <Country {...props} />}
+        />
       </Router>
     </div>
   );
-}
+};
 const mapDispatchToProps = {
-  initializer,
-}
+  initializer
+};
 const mapStateToProps = (state: any) => {
   return {
-    countries: state.countries,
-  }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+    countries: state.countries
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,43 +1,81 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import '../css/menubar.css'
-import HamburgerButton from './HamburgerButton'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/menubar.css';
+import HamburgerButton from './HamburgerButton';
 
 interface Props {
-setSearch:any
-toggleSideDrawer:any
+  setSearch: any;
+  toggleSideDrawer: any;
 }
 
-const Menubar: React.FC<Props> = (props) => {
-    const handleChange = (e: any) => {
-        e.preventDefault()
-        props.setSearch(e.target.value)
+const Menubar: React.FC<Props> = props => {
+  const handleChange = (e: any) => {
+    e.preventDefault();
+    props.setSearch(e.target.value);
+  };
+  const clearFilterInput = () => {
+    const doc = document.getElementById('filterinput') as HTMLInputElement;
+    if (doc) {
+      props.setSearch('');
+      doc.value = '';
     }
-    const clearFilterInput=()=>{
-      const doc=document.getElementById('filterinput') as HTMLInputElement
-      if(doc){
-        props.setSearch('')
-        doc.value=''
-      }
-    }
-    
-    return (<div >
-        <header className='menubar'>
-            <nav className='menubar_navigation'>
-                <HamburgerButton className='hamburgerMenu' onClick={props.toggleSideDrawer}></HamburgerButton>
-                <Link onClick={clearFilterInput} className='logo' to='/'>Countries</Link>
-                <Link onClick={clearFilterInput} className='menuLinkItem' to='/Africa'>Africa</Link>
-                <Link onClick={clearFilterInput} className='menuLinkItem' to='/Europe'>Europe</Link>
-                <Link onClick={clearFilterInput} className='menuLinkItem' to='/Americas'>Americas</Link>
-                <Link onClick={clearFilterInput} className='menuLinkItem' to='/Asia'>Asia</Link>
-                <Link onClick={clearFilterInput} className='menuLinkItem' to='/Oceania'>Oceania</Link>
+  };
 
-                <Link onClick={clearFilterInput} className='menuLinkItem' to='/All'>All</Link>
-                <input id='filterinput' placeholder='filter by name' onChange={handleChange}></input>
-            </nav>
-        </header>
-        
-    </div>)
-}
+  return (
+    <div>
+      <header className="menubar">
+        <nav className="menubar_navigation">
+          <HamburgerButton
+            className="hamburgerMenu"
+            onClick={props.toggleSideDrawer}
+          ></HamburgerButton>
+          <Link onClick={clearFilterInput} className="logo" to="/">
+            Countries
+          </Link>
+          <Link
+            onClick={clearFilterInput}
+            className="menuLinkItem"
+            to="/Africa"
+          >
+            Africa
+          </Link>
+          <Link
+            onClick={clearFilterInput}
+            className="menuLinkItem"
+            to="/Europe"
+          >
+            Europe
+          </Link>
+          <Link
+            onClick={clearFilterInput}
+            className="menuLinkItem"
+            to="/Americas"
+          >
+            Americas
+          </Link>
+          <Link onClick={clearFilterInput} className="menuLinkItem" to="/Asia">
+            Asia
+          </Link>
+          <Link
+            onClick={clearFilterInput}
+            className="menuLinkItem"
+            to="/Oceania"
+          >
+            Oceania
+          </Link>
 
-  export default Menubar
+          <Link onClick={clearFilterInput} className="menuLinkItem" to="/All">
+            All
+          </Link>
+          <input
+            id="filterinput"
+            placeholder="filter by name"
+            onChange={handleChange}
+          ></input>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Menubar;
